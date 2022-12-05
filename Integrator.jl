3cbcf126b914e190561cae7a825c20607d5f5963
Marmik Thakkar <marmik18@gmail.com>
2022-12-06 00:10:22 +0530

    completed euler integration

--- Integrator.jl
+++ Integrator.jl
@@ -18,7 +18,7 @@ mutable struct Integrator
 end
 
 ## run one integration time step
-function run_step(int::Integrator, type, pendulum)
+function run_step(int::Integrator, type::String, pendulum::Math_pendulum)
     if type == "euler"
         run_euler_step(int, pendulum)
     elseif type == "central_diff"
@@ -29,13 +29,20 @@ function run_step(int::Integrator, type, pendulum)
 end
 
 ## euler integration time step (homework)
-function run_euler_step(int::Integrator, pendulum)
-    println("Running euler step")
+function run_euler_step(int::Integrator, pendulum::Math_pendulum)
+    # print(".")
+    c = 5.0
     ###### (homework) ######
+    omega_sq = pendulum.g / pendulum.l
+    phi_dot_dot = -c * pendulum.phi_dot - omega_sq * pendulum.phi
+    phi_i_plus_1 = pendulum.phi + int.delta_t * pendulum.phi_dot
+    phi_dot_i_plus_1 = pendulum.phi_dot + int.delta_t * phi_dot_dot
+    pendulum.phi = phi_i_plus_1
+    pendulum.phi_dot = phi_dot_i_plus_1
 end
 
 ## central difference time step (homework)
-function run_central_diff_step(int::Integrator, pendulum)
+function run_central_diff_step(int::Integrator, pendulum::Math_pendulum)
     println("Running central difference step")
     ###### (homework) ######
 end
--- main.jl
+++ main.jl
@@ -10,17 +10,22 @@ using Plots, Printf
 using DelimitedFiles
 
 ## booleans
-show_video = false
+show_video = true
+pendulum_animation = Animation()
 
 ## starting the file
 println("-- pendulum euler --")
 
 ## load pendulum
 include("Dynsys.jl")
-pendulum = Dynsys.Math_pendulum(1.0, 10.0, 1.0, 0.5, 0.0)
+pendulum = Dynsys.Math_pendulum(10.0 / 300.0, 10.0, 1.0, 0.5, 0.0)
 
 ## load integrator and memory for the results
-Integ = Dynsys.Integrator(1.0e-3,5)
+delta_t = 1.0e-3
+timesteps = 2000
+type = "euler" # or "central_diff"
+
+Integ = Dynsys.Integrator(delta_t, timesteps)
 Integ.res_phi = zeros(Integ.timesteps)
 Integ.res_phi_dot = zeros(Integ.timesteps)
 
@@ -35,15 +40,26 @@ for i in 1:Integ.timesteps
     # (homework)
     # plot the state
     fig = Dynsys.create_fig(pendulum)
-    Dynsys.run_step(Integ, "euler", pendulum)
+    Dynsys.run_step(Integ, type, pendulum)
     Dynsys.plot_state(pendulum)
     display(fig)
+    frame(pendulum_animation)
     # save the step
     Integ.res_phi[i] = pendulum.phi
     Integ.res_phi_dot[i] = pendulum.phi_dot
 end
 
+x = LinRange(0, timesteps * delta_t, timesteps)
+display(plot(x, Integ.res_phi, xlabel="time", ylabel="position"))
+savefig("output/$(type)_position.png")
+
+display(plot(x, Integ.res_phi_dot, xlabel="time", ylabel="velocity"))
+savefig("output/$(type)_velocity.png")
+
+if show_video
+    gif(pendulum_animation, "output/$(type)_pendulum.gif")
+end
 ######## Homework
-# implement the euler integration step
+# Done: implement the euler integration step
 # implement the central difference integration step
 # plot coordinate phi and the time derivative phi_dot

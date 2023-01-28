02f38e6712d19c5db6be63b2c929457bb240964d
Marmik Thakkar <marmik18@gmail.com>
2023-01-28 21:17:06 +0100

    working as expected

--- Neural_Network.jl
+++ Neural_Network.jl
@@ -2,16 +2,17 @@ using CSV
 using DataFrames, Random
 using Flux, Statistics, ProgressMeter
 using Plots
+using Dates
 include("Utils.jl")
 
 type = "euler" # or "central_diff"
-epochs = 30000
-max_learning_rate = 0.0001
+epochs = 15000
+max_learning_rate = 0.01
 
 l = 10.0 / 300.0
 g = 10.0
-omega_sq = g / l
 c = 5.0
+delta_t = 1.0e-3
 
 df = DataFrame(CSV.File("output/$(type).csv", types=[Float32, Float32, Float32]))
 # train has first 35% of the datapoints
@@ -19,7 +20,7 @@ train, test = splitdf(df, 0.35)
 
 # select timesteps at an interval for complete dataset.
 timesteps = select(train, ["timestep"])
-interval = 20
+interval = 50
 physics_train_data = transpose(Matrix(timesteps[1:interval:size(timesteps)[1], :]))
 
 # select all fields for data and target
@@ -30,75 +31,48 @@ test_data = transpose(Matrix(select(test, ["timestep"])))
 test_target = transpose(Matrix(select(test, ["phi"])))
 
 model = Chain(
-    Dense(1 => 12, tanh),
-    Dense(12 => 12, sigmoid),
-    Dense(12 => 1, tanh),
+    Dense(1 => 32, tanh),
+    Dense(32 => 32, sigmoid),
+    Dense(32 => 1),
 )
 
 out = model(train_data)
 loader = Flux.DataLoader((train_data, train_target), batchsize=64, shuffle=true)
 pars = Flux.params(model)
-opt = Flux.AdaMax(max_learning_rate)
-
-# println(pars)
+opt = Flux.Adam(max_learning_rate)
 
 # Training loop, using the whole data set 10 times:
 losses = []
+prediction_animation = Animation()
 @showprogress for epoch in 1:epochs
-    i=1
     for (x, y) in loader
-        j=1
         loss, grad = Flux.withgradient(pars) do
             # Evaluate model and loss inside gradient context:
             y_hat = model(x)
             loss_mse = Flux.mse(y_hat, y)
 
             # compute the "physics loss"
-            y_hat_physics = model(physics_train_data)
+            y_hat_physics = model(hcat(train_data, test_data))
 
-            # x(t) = sum(y_hat_physics ./ t)
-            # dx(t) = gradient(x, t)[1]
+            loss_residual = PhysicsLoss(y_hat_physics, l, g, c, delta_t)
 
-            something = derivative(y_hat_physics, physics_train_data)
-            # dxx = derivative(dx, physics_train_data)
-            # print(dx)
-            # dx(xt)
-
-            # d2x(t) = gradient(dx, t)[1]
-            # d2x(physics_train_data)
-            # g = @diff sum(y_hat_physics ./ physics_train_data)
-            # y_hat_physics_dot = grad(g, physics_train_data)
-            
-            # g_1 = @diff sum(y_hat_physics_dot ./ physics_train_data )
-            # y_hat_physics_dot_dot = grad(g_1, physics_train_data)
-            # y_hat_physics_dot = grad(y_hat_physics, physics_train_data)
-            # y_hat_physics_dot_dot = grad(y_hat_physics, y_hat_physics_dot)
-            # y_hat_physics_dot   = torch.autograd.grad(yhp, x_physics, torch.ones_like(yhp), create_graph=True)[0] # computes dy/dx
-            # y_hat_physics_dot_dot = torch.autograd.grad(dx,  x_physics, torch.ones_like(dx),  create_graph=True)[0] # compu
-
-            # physics = y_hat_physics_dot_dot + c * y_hat_physics_dot + omega_sq * y_hat_physics # computes the residual of the 1D harmonic oscillator differential equation
-            # loss_physics = (1e-4) * mean(physics^2)
-
-            # if epoch == 1 && i == 1  
-            #     println(loss_physics)
-            # end
-            total_loss = loss_mse
+            total_loss = loss_mse + loss_residual
             return total_loss
         end
-        j+=1
         Flux.update!(opt, pars, grad)
         push!(losses, loss)  # logging, outside gradient context
     end
-    i+=1
+    y_hat_physics = model(hcat(train_data, test_data))
+    plot(df.timestep, df.phi, label="solved with eq", title="Epoch: $(epoch)")
+    plot!(transpose(hcat(train_data, test_data)), transpose(y_hat_physics), label="predictions")
+    frame(prediction_animation)
 end
 
-plot(losses; xaxis=(:log10, "iteration"),
-    yaxis="loss", label="per batch")
-n = length(loader)
-plot!(n:n:length(losses), mean.(Iterators.partition(losses, n)),
-    label="epoch mean", dpi=200)
 
-# savefig("output/$(type)_loss.png")
+
+plot(losses; xaxis="iteration",
+    yaxis="loss", label="per batch")
+savefig("output/nn_loss_$(now()).png")
 
 predictions_train_target = model(train_data)
 predictions = model(test_data)
@@ -109,4 +83,5 @@ plot!(transpose(test_data), transpose(predictions), label="predictions")
 plot!(transpose(train_data), transpose(train_target), label="train")
 plot!(transpose(train_data), transpose(predictions_train_target), label="train predictions")
 
-# savefig("output/$(type)_prediction.png")
+savefig("output/nn_prediction_$(now()).png")
+gif(prediction_animation, "output/nn_animation_60fps.gif", fps=60)
--- Utils.jl
+++ Utils.jl
@@ -9,22 +9,24 @@ function splitdf(df, pct)
     return view(df, sel, :), view(df, .!sel, :)
 end
 
-function derivative(x, t)
-    input = deepcopy(x)
-    time = deepcopy(t)
-    
-    limit = size(input)[2]
+function PhysicsLoss(phi, l, g, c, delta_t)
 
-    res = zeros(size(input))
-
-    for i in 1:limit
-        if i == limit
-            res[i] = input[i] / time[i] - time[i-1]
-        else
-            res[i] = (input[i+1] - input[i]) / time[i+1] - time[i]
-        end
+    # Calculate loss as the mean squared error (MSE) between the predicted and target values
+    loss = []
+    stepsize = 1
+    for i in 1+stepsize:50:length(phi)-stepsize
+        phi_dot = (phi[i+stepsize] - phi[i-stepsize]) / (2 * delta_t * stepsize)
+        phi_dotdot = (phi[i+stepsize] - 2 * phi[i] + phi[i-stepsize]) / (delta_t * stepsize)^2
+        r = phi_dotdot + c * phi_dot + g / l * phi[i]
+        loss = vcat(loss, [r])
     end
+    # for i in 1+stepsize:length(phi)-stepsize
+    #     phi_dot = (phi[i+stepsize] - phi[i-stepsize]) / (2 * delta_t * stepsize)
+    #     phi_dotdot = (phi[i+stepsize] - 2 * phi[i] + phi[i-stepsize]) / (delta_t * stepsize)^2
+    #     r = phi_dotdot + c * phi_dot + g / l * phi[i]
+    #     loss = vcat(loss, [r])
+    # end
 
-    return copy(res)
+    return (5e-4) * mean(loss .^ 2)
 end
 

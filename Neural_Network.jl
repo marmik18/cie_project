95cd567e74da0b094d0c4c65b6f1c3fbc13b6fdb
Marmik Thakkar <marmik18@gmail.com>
2023-01-29 13:33:24 +0100

    updated to handle animation outside epoch loop

--- Neural_Network.jl
+++ Neural_Network.jl
@@ -19,7 +19,7 @@ df = DataFrame(CSV.File("output/$(type).csv", types=[Float32, Float32, Float32])
 train, test = splitdf(df, 0.35)
 
 # select timesteps at an interval for complete dataset.
-timesteps = select(train, ["timestep"])
+timesteps = select(df, ["timestep"])
 interval = 50
 physics_train_data = transpose(Matrix(timesteps[1:interval:size(timesteps)[1], :]))
 
@@ -43,8 +43,9 @@ opt = Flux.Adam(max_learning_rate)
 
 # Training loop, using the whole data set 10 times:
 losses = []
-prediction_animation = Animation()
+prediction_per_epoch = []
 @showprogress for epoch in 1:epochs
+    y_hat_physics = missing
     for (x, y) in loader
         loss, grad = Flux.withgradient(pars) do
             # Evaluate model and loss inside gradient context:
@@ -62,26 +63,29 @@ prediction_animation = Animation()
         Flux.update!(opt, pars, grad)
         push!(losses, loss)  # logging, outside gradient context
     end
-    y_hat_physics = model(hcat(train_data, test_data))
-    plot(df.timestep, df.phi, label="solved with eq", title="Epoch: $(epoch)")
-    plot!(transpose(hcat(train_data, test_data)), transpose(y_hat_physics), label="predictions")
-    frame(prediction_animation)
+    push!(prediction_per_epoch, y_hat_physics)
 end
 
 
 
-plot(losses; xaxis="iteration",
-    yaxis="loss", label="per batch")
-savefig("output/nn_loss_$(now()).png")
+display(plot(losses; xaxis="iteration",
+    yaxis="loss", label="per batch"))
+savefig("output/nn_loss_$(now()).svg")
 
-predictions_train_target = model(train_data)
+predictions_train = model(train_data)
 predictions = model(test_data)
 
 plot(df.timestep, df.phi, xaxis="timestep",
     yaxis="phi", label="solved with $(type) equation")
 plot!(transpose(test_data), transpose(predictions), label="predictions")
 plot!(transpose(train_data), transpose(train_target), label="train")
-plot!(transpose(train_data), transpose(predictions_train_target), label="train predictions")
+display(plot!(transpose(train_data), transpose(predictions_train), label="train predictions"))
 
-savefig("output/nn_prediction_$(now()).png")
+savefig("output/nn_prediction_$(now()).svg")
+
+prediction_animation = @animate for epoch ∈ 1000:5000
+    plot(df.timestep, df.phi, label="solved with eq", title="Epoch: $(epoch)", ylim=(-0.4, 0.6))
+    plot!(transpose(hcat(train_data, test_data)), transpose(prediction_per_epoch[epoch]), label="predictions")
+    frame(prediction_animation)
+end
 gif(prediction_animation, "output/nn_animation_60fps.gif", fps=60)
